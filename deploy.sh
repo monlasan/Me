f51819ff0d8ffa8157d17053593da3435c463814
Paul Smith <pzannou511@gmail.com>
2022-02-28 00:47:33 +0100

    Removed react router

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,13 @@ set -e
 yarn build
 
 # navigate into the build output directory
-cd dist
+#cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+git add dist -f
+git commit -m "deploy"
+git subtree push --prefix dist origin gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+#cd -
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route
+    <App />
+    {/* <HashRouter basename='http://localhost:3000/Me/Me/'> */}
+    {/* <Routes> */}
+    {/* <Route index element={<App />} /> */}
+    {/* <Route
           path='*'
           element={
             <div className='w-full text-center min-h-screen bg-gray-100 grid place-items-center'>
@@ -23,8 +24,9 @@ ReactDOM.render(
             </div>
           }
         />
-      </Routes>
-    </BrowserRouter>
+      </Routes> 
+    </HashRouter>
+      */}
   </React.StrictMode>,
   document.getElementById('root')
 );
